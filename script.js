46d4e0caa0aff82b954e3f987387abe69723a7e2
Maria Kireeva <mariakmk037@gmail.com>
2023-02-08 18:29:50 +0200

    made array

--- script.js
+++ script.js
@@ -1,75 +1,27 @@
-let userEmail = prompt('Write your email');
-let userPassword = prompt('Write your password');
-let attempts = 3;
-let error = '';
+let arr = [];
+let arrLength;
+arrLength = prompt('Write the length of array');
 
-
-while ((userEmail.length > 15 || userEmail.startsWith(' ') || userEmail.endsWith(' ') || userEmail.startsWith('@') || userEmail.endsWith('@') || !userEmail.includes('@') || !userEmail.endsWith('.com') || userPassword.search(/[A-Z]/g) == -1 || userPassword.length < 4 || userPassword.length > 12) &&  attempts > 0) {
-    
-    
-    if (userEmail.length > 15) {
-        error = error + `* Email should consist less than 15 symbols
-        `
-    }   
-
-    if (userEmail.startsWith(' ')) {
-        error = error + `* Email must not start with a gap
-        `
+while (arrLength == null || arrLength > 10 || arrLength < 2) {
+    if (arrLength <= -2 && arrLength >= -10) {
+        arrLength = Math.abs(arrLength);
+        break;
     }
 
-    if (userEmail.endsWith(' ')) {
-        error = error + `* Email must not end with a gap
-        `
-    }
-
-    if (userEmail.startsWith('@') || userEmail.endsWith('@')) {
-        error = error + `* Email must not start/end with '@'
-        `
-    }
-
-    if (!userEmail.includes('@')) {
-        error = error + `* Email must include at least one '@'
-        `
-    }
-
-    if (!userEmail.endsWith('.com')) {
-        error = error + `* You have to end your email with ' .com'
-        `
-    }
-
-    if (userPassword.length < 4) {
-        error = error + `* Password should not be less than 4 symbols
-        `;
-    }
-    if (userPassword.length > 12) {
-        error = error + `* Password should not be more than 12 symbols
-        `;
-    }
-    if (userPassword.search(/[A-Z]/g) == -1) {
-        error = error + `* Password should include at least one capital letter
-        `;
-    }
-
-    alert(`You have ${attempts} more attempts.
-        ${error}`);
-        error = '';
-    attempts = attempts - 1;
-    userEmail = prompt('Write your email again');
-    userPassword = prompt('Write your password again');
+    arrLength = prompt('Write the length of array again');
 }
 
-if (attempts <= 0) {
-    alert(`Sorry, you don't have any more attempts.`)
-    error = true;
+if (!((arrLength ^ 0) === arrLength)) {
+    arrLength = Math.round(arrLength);
 }
+arr.length = arrLength;
 
-
-if(error) {
-    document.write(`Your account was not registered.`);
-} else {
-    document.write(`<pre>
-    Your account succesfully registered!
-    email: ${userEmail}
-    password: ${userPassword}
-    </pre>`);
+for (let i = 0; i < arr.length; i++) {
+    arr[i] = Math.round(Math.random()*10);;
 }
+console.log(arr);
+let sum = 1;
+for (let el of arr) {
+    sum = sum * el;
+}
+console.log(sum);
