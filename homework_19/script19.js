bd1d9ed70362194d31ee04d59691a89bf45542b1
Maria Kireeva <mariakmk037@gmail.com>
2023-04-16 12:40:58 +0300

    delete hw19

--- homework_19/script19.js
+++
@@ -1,8 +0,0 @@
-import { api } from "./api/api.js";
-import { renderExistedHeroes } from "./feature/renderExisted.js";
-import { renderAddForm } from "./feature/renderAddForm.js";
-renderAddForm()
-
-api.GET_EXISTED().then((res) => {
-  res.forEach((item) => renderExistedHeroes(item))
-})
